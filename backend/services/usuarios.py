b6a22f24e4e359762bfbdda621d70c548a6af10c
migueel15 <miguelangeldorado10@gmail.com>
2024-12-10 11:54:54 +0100

    usuarios implementado

--- backend/services/usuarios.py
+++ backend/services/usuarios.py
@@ -1,44 +1,45 @@
-# import json
-# from bson import ObjectId
-# from fastapi import APIRouter, HTTPException
-# from pymongo import MongoClient
-# from dotenv import load_dotenv
-# from models.user import User, UserList, UserNew, UserUpdate
-# from typing import Optional
-# import os
-#
-# load_dotenv()
-#
-# MONGO_URL = os.getenv("MONGO_URL")
-#
-# usuarios_router = APIRouter(prefix="/parcial/usuarios", tags=["usuarios"])
-#
-# # Configuración de MongoDB
-# client = MongoClient(MONGO_URL)
-# db = client.laWikiv2
-# usuarios = db.usuarios
-#
-#
-# # GET /usuarios
-# @usuarios_router.get("/", response_model=UserList)
-# def get_users(
-#     name: Optional[str] = None, email: Optional[str] = None, role: Optional[str] = None
-# ):
-#     query = {}
-#     if name:
-#         query["name"] = {"$regex": name, "$options": "i"}
-#     if email:
-#         query["email"] = {"$regex": email, "$options": "i"}
-#     if role:
-#         query["role"] = {"$regex": role, "$options": "i"}
-#
-#     try:
-#         users_data = usuarios.find(query).to_list(1000)
-#         return UserList(users=users_data)
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=400, detail=f"Error al buscar los usuarios: {str(e)}"
-#         )
+import json
+import os
+from typing import Optional
+
+from bson import ObjectId
+from dotenv import load_dotenv
+from fastapi import APIRouter, HTTPException
+from models.user import User, UserList, UserNew, UserUpdate
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+usuarios_router = APIRouter(prefix="/parcial/usuarios", tags=["usuarios"])
+
+# Configuración de MongoDB
+client = MongoClient(MONGO_URL)
+db = client.laWikiv2
+usuarios = db.usuarios
+
+
+# GET /usuarios
+@usuarios_router.get("/", response_model=UserList)
+def get_users(
+    name: Optional[str] = None, email: Optional[str] = None, role: Optional[str] = None
+):
+    query = {}
+    if name:
+        query["name"] = {"$regex": name, "$options": "i"}
+    if email:
+        query["email"] = {"$regex": email, "$options": "i"}
+    if role:
+        query["role"] = {"$regex": role, "$options": "i"}
+
+    try:
+        users_data = usuarios.find(query).to_list(1000)
+        return UserList(users=users_data)
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"Error al buscar los usuarios: {str(e)}"
+        )
 #
 #
 # # GET /usuarios/<id>
