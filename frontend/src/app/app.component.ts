460cae2ccf0eb158087479155c6b4888e4ce76be
migueel15 <miguelangeldorado10@gmail.com>
2024-12-10 13:38:43 +0100

    aaaaaaaaa

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SubirImagenesComponent } from './modules/imagenes/subir-imagenes.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SubirImagenesComponent],
+  imports: [
+    RouterOutlet,
+    SubirImagenesComponent,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:4200'], // URLs protegidas
+        sendAccessToken: true, // Enviar token en el header
+      },
+    }),
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
