3ab43ef2fc08a8edf7313a0a1480182c742bc2c0
migueel15 <miguelangeldorado10@gmail.com>
2024-12-10 17:31:08 +0100

    cambios

--- frontend/src/app/modules/auth/auth.service.ts
+++ frontend/src/app/modules/auth/auth.service.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
-import { authConfig } from './auth.config';
+import { Injectable } from '@angular/core'
+import { OAuthService } from 'angular-oauth2-oidc'
+import { AuthConfig } from 'angular-oauth2-oidc'
+import { environmet } from '../../../environments/environment'
 
-@Component({
-  selector: 'app-root',
-  template: `<button (click)="login()">Login</button>`,
+const authConfig: AuthConfig = {
+  issuer: 'https://accounts.google.com', // URL del proveedor de OAuth 2.0
+  clientId: environmet.CLIENT_ID_MIGUEL,
+  redirectUri: window.location.origin, // URL de redirección (debe coincidir con la registrada en el proveedor)
+  responseType: 'code', // Tipo de respuesta (usualmente "code" para el flujo de autorización)
+  scope: 'openid profile email', // Scopes autorizados
+  showDebugInformation: true, // Opcional, para debug en consola
+  waitForTokenInMsec: 2000,
+}
+
+@Injectable({
+  providedIn: 'root',
 })
-export class AppComponent implements OnInit {
-  constructor(private oauthService: OAuthService) {}
-
-  ngOnInit() {
-    this.oauthService.configure(authConfig);
-    this.oauthService.loadDiscoveryDocumentAndTryLogin(); // Carga metadatos y verifica autenticación
+export class AuthService {
+  constructor(private oAuthService: OAuthService) {
+    oAuthService.configure(authConfig)
   }
 
-  login() {
-    this.oauthService.initLoginFlow(); // Redirige al proveedor para iniciar sesión
-  }
-
-  logout() {
-    this.oauthService.logOut(); // Cierra sesión
-  }
-
-  get token() {
-    return this.oauthService.getAccessToken(); // Obtiene el token de acceso
-  }
+  //
+  // login() {
+  //   this.oauthService.initLoginFlow(); // Redirige al proveedor para iniciar sesión
+  // }
+  //
+  // logout() {
+  //   this.oauthService.logOut(); // Cierra sesión
+  // }
+  //
+  // get token() {
+  //   return this.oauthService.getAccessToken(); // Obtiene el token de acceso
+  // }
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environmet = {
-  CLIENT_ID_PEDRO: ""
+  CLIENT_ID_PEDRO: '',
+  CLIENT_ID_MIGUEL:
+    '279955462362-vh3h4gquqkccipad9gd0i09h8724shm5.apps.googleusercontent.com',
 }
